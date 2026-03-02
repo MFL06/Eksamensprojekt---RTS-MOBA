707fd478856671d8af4708072d634a851bdccb92
Chenyali06 <alexander06chen@gmail.com>
2026-03-02 12:56:00 +0100

    Collision checks

---
+++ Alex/tutorial.js
@@ -0,0 +1,31 @@
+import { circleoverlap} from ".collissions.js";
+
+const circleA = {x : 20, y : 50, radius : 30};
+const circleB = {x : 20, y : 10, radius : 10};
+
+function drawCirlce(context, circle, color, id){
+    const {x, y, radius} = circle;
+
+    context.lineWdith = 0;
+    context.fillstyle = color;
+
+    context.beginpath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.fill();
+
+    context.font = `${radius / 2}px Arial`;
+    context.style = 'white';
+    context.textBaseline = 'middle';
+    context.fillText(id, x, y);
+}
+
+
+
+export function tutorial(context) {
+    const result = circleoverlap (circleA, circleB);
+
+    context.ClearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    drawCirlce(context, circleA, 'rgb(47 66 212)','A')
+    drawCirlce(context, circleB, 'rgb(217 84 54)','B')
+}

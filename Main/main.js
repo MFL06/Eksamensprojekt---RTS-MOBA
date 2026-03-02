daec937849d87de5242da5de6c82acb49a058101
MFL06 <mikkelljungbeck@gmail.com>
2026-03-02 10:47:28 +0100

    fil fix

---
+++ Main/main.js
@@ -0,0 +1,23 @@
+function setup(){
+    createCanvas(800, 800)
+}
+
+function draw(){
+
+}
+
+class Vek{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+}
+
+class Char{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+        this.r = 30
+        this.vek = new Vek(0, 0)
+    }
+}

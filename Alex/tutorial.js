f58b335ee1c3e97c9b855bbdd15cfe0b7a6738e0
Chenyali06 <alexander06chen@gmail.com>
2026-03-04 10:42:18 +0100

    I AM COOKING

--- Alex/tutorial.js
+++ Alex/tutorial.js
@@ -1,13 +1,13 @@
 import { circleoverlap} from "./collisions.js";
 
-const circleA = {x : 20, y : 50, radius : 30};
-const circleB = {x : 20, y : 10, radius : 10};
+const circleA = {x : 600, y : 500, radius : 100};
+const circleB = {x : 700, y : 500, radius : 150};
 
-function drawCirlce(context, circle, color, id){
+function drawCircle(context, circle, color, id){
     const {x, y, radius} = circle;
 
-    context.lineWdith = 0;
-    context.fillstyle = color;
+    context.lineWidth = 0;
+    context.fillStyle = color;
 
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI);
@@ -19,13 +19,23 @@ function drawCirlce(context, circle, color, id){
     context.fillText(id, x, y);
 }
 
+function drawCollisonmessage(context, hasCollided){
+    context.font = `2vw Arial`;
+    context.fillStyle = 'white';
+    context.textBaseline = 'middle';
+    context.fillText(
+        `Circles are overlapping: ${hasCollided}`,
+         context.canvas.width / 2, 10,);
+}
 
 
 export function tutorial(context) {
-    const result = circleoverlap (circleA, circleB);
+    const hasCollided = circleoverlap (circleA, circleB);
 
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     
-    drawCirlce(context, circleA, 'rgb(47 66 212)','A')
-    drawCirlce(context, circleB, 'rgb(217 84 54)','B')
+    drawCircle(context, circleA, 'rgb(47 66 212)','A')
+    drawCircle(context, circleB, 'rgb(217 84 54)','B')
+
+    drawCollisonmessage(context, hasCollided);
 }

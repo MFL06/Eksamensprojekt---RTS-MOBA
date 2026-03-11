e4be7d84ba0c30927926c927fb4af28fdb43168c
MFL06 <mikkelljungbeck@gmail.com>
2026-03-11 14:15:55 +0100

    Local map update

--- Mikkel/Map.js
+++ Mikkel/Map.js
@@ -3,27 +3,81 @@ var tileSize = 80;
 var rows = 10;
 var cols = 10;
 let map = [];
+var cardrows = 4
+var cardsizew = 150
+var cardsizeh = 200
+var cardgap = 20
+let cards = []
 
 let obj1
 
 let charList = []
 
 
-function setup() {
-  createCanvas(800, 900);
-  rectMode(CENTER); // draw from center instead of top-left
-  // Create 2D map with center coordinates for each square
-  for (let r = 0; r < rows; r++) {
+function cardcords(){
+  for (let crd = 0; crd < cardrows; crd++){
+    cards[crd]={
+      x: crd * (cardsizew + cardgap) + cardsizew / 2, // center X - spread horizontally with gap
+      y: 975, // fixed Y position at bottom
+      color: "#d2aa77",
+      width: cardsizew,
+      height: cardsizeh,
+    }
+  }
+}
+
+function drawcards(){
+  for (let crd = 0; crd < cards.length; crd++) {
+      let card = cards[crd];
+      fill(card.color);
+      stroke(0); // optional: add grid lines
+      rect(card.x + 70, card.y, card.width, card.height);
+  }
+}
+
+
+function cords(){
+   for (let r = 0; r < rows; r++) {
     map[r] = [];
     for (let c = 0; c < cols; c++) {
-      if(r == 1 && c == 1) {
+      // Bridge
+      if(r == 5 && c == 1 || r == 4 && c == 1 || r == 4 && c == 8 || r == 5 && c == 8) {
         map[r][c] = {
         x: c * tileSize + tileSize / 2, // center X
         y: r * tileSize + tileSize / 2, // center Y
-        color: "green",
+        color: "grey",
         walkable: true // optional: useful for pathfinding later
         }
-      }else{
+      }
+      // water
+      else if(r == 4 || r == 5){
+         map[r][c] = {
+        x: c * tileSize + tileSize / 2, // center X
+        y: r * tileSize + tileSize / 2, // center Y
+        color: "blue",
+        walkable: false // optional: useful for pathfinding later
+      }
+      // Towers
+      }else if(r == 8 && c == 1 || r == 8 && c == 8 || r == 1 && c == 1 || r == 1 && c == 8){
+        map[r][c] = {
+        x: c * tileSize + tileSize / 2, // center X
+        y: r * tileSize + tileSize / 2, // center Y
+        color: "#c0c0c0",
+        tower: true,
+        walkable: false // optional: useful for pathfinding later
+      }
+      // King tower
+      }else if(r == 9 && c == 4 || r == 9 && c == 5 || r == 0 && c == 4 || r == 0 && c == 5){
+        map[r][c] = {
+        x: c * tileSize + tileSize / 2, // center X
+        y: r * tileSize + tileSize / 2, // center Y
+        color: "#EFBF04",
+        ktower: true,
+        walkable: false // optional: useful for pathfinding later
+      }
+      }
+      else{  
+        // Grass
         map[r][c] = {
         x: c * tileSize + tileSize / 2, // center X
         y: r * tileSize + tileSize / 2, // center Y
@@ -31,12 +85,16 @@ function setup() {
         walkable: true // optional: useful for pathfinding later
         }
       }
-      
     }
   }
-  console.log(map); // check the map structure
-  //obj1 = new Char(map[2][2].x, map[2][2].y)
-  //charList.push(obj1)
+}
+
+function setup() {
+  createCanvas(800, 1100);
+  rectMode(CENTER); // draw from center instead of top-left
+  // Create 2D map with center coordinates for each square
+  cords()
+  cardcords()
 }
 
 function dragChar(array){
@@ -68,35 +126,10 @@ function dragChar(array){
   }
 
 }
-/*
-function findSquare(array){
-  const mx = mouseX
-  const my = mouseY
-  if(mx < width && mx > 0 && my < 800 && my > 0){
-    let list = [] 
-    let diffX
-    let diffY
-    let cords
-    for(let r = 0; r < rows; r ++){
-      for(let c = 0; c < cols; c ++){
-        diffX = array[r][c].x - mx
-        diffY = array[r][c].y - my + 40
-        list.push(Math.hypot(diffX, diffY))
-        if(list.length > 1 && list[list.length - 1] < 80 && list[list.length - 1] < list[list.length - 2]){
-          cords = {x: array[r][c].x, y: array[r][c].y}
-        }else if(list.length == 1){
-          cords = {x: array[r][c].x, y: array[r][c].y}
-        }
-      }
-    }
-    return cords
-  }
-  else{
-    cords = {x: mx, y: my}
-    return cords
-  } 
-}
-*/
+
+
+
+
 function mousePressed(){
   const mx = mouseX
   const my = mouseY
@@ -162,13 +195,16 @@ function draw() {
     }
   }
 
+  
+
   circle(200, 840, 50)
   circle(280, 840, 50)
   circle(360, 840, 50)
   if(charList.length > 0){
     dragChar(map)
   }
-  
+
+  drawcards()
   showAll(charList)
   //dragChar(map)
 }

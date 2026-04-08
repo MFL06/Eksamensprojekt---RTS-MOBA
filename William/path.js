1e8b52138a3be612477ba7a3fb867a59781ae9a8
H3rsk <will.herskind@gmail.com>
2026-04-08 13:57:29 +0200

    kollision, path, map basically done'ish

--- William/path.js
+++ William/path.js
@@ -1,32 +1,32 @@
 function moveTowardsBridge(char) {
     if (char.isDragged) return
- 
+
     let bridge1X = 1 * tileSize + tileSize / 2
     let bridge2X = 8 * tileSize + tileSize / 2
     let targetX = (Math.abs(char.x - bridge1X) < Math.abs(char.x - bridge2X)) ? bridge1X : bridge2X
     let targetY = 5 * tileSize + tileSize / 2  // Det første grå felt (r=5)
- 
+
     if (char.y > targetY) {
         // Gå diagonalt mod broen
         let dx = targetX - char.x
         let dy = targetY - char.y
         let dist = Math.hypot(dx, dy)
-        let stepX = (dx / dist) * 2
-        let stepY = (dy / dist) * 2
- 
+        let stepX = (dx / dist) * char.speed
+        let stepY = (dy / dist) * char.speed
+
         // Prøv fuld bevægelse
         char.x += stepX
         char.y += stepY
- 
+
         // Hvis kollision, rul tilbage og prøv kun X eller kun Y
         if (checkKollision(char)) {
             char.x -= stepX
             char.y -= stepY
- 
+
             // Prøv kun X
             char.x += stepX
             if (checkKollision(char)) char.x -= stepX
- 
+
             // Prøv kun Y
             char.y += stepY
             if (checkKollision(char)) char.y -= stepY
@@ -34,10 +34,10 @@ function moveTowardsBridge(char) {
     } else {
         // Broen nået – gå lige op
         char.x = targetX
-        char.y -= 2
- 
+        char.y -= char.speed
+
         if (checkKollision(char)) {
-            char.y += 2
+            char.y += char.speed
         }
     }
 }
